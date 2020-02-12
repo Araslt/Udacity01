807b3f54061e77932ff3e66d6641ae0ccb8b596b
ArasLT <buk.sveikas@gmail.com>
2020-02-12 19:20:55 +0200

    Lesson 1: Making an App Interactive: Part 1

---
+++ JustJava/app/src/main/java/lt/mosaica/justjava/MainActivity.java
@@ -0,0 +1,63 @@
+package lt.mosaica.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity<displayPrice> extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        displayPrice(2 * 5);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        //to do
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        quantity = 1 + quantity;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+}
