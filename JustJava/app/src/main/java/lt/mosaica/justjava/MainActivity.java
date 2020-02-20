33134c74067428dff519192dd819ef7f6482b548
ArasLT <buk.sveikas@gmail.com>
2020-02-20 20:35:47 +0200

    Just Java method before  more concise code, you can use the += operator

--- JustJava/app/src/main/java/lt/mosaica/justjava/MainActivity.java
+++ JustJava/app/src/main/java/lt/mosaica/justjava/MainActivity.java
@@ -16,18 +16,34 @@ public class MainActivity<displayPrice> extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //displayPrice(2 * 5);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity *5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank You!";
+        int price = calculatePrice();
+        String priceMessage = "Total $" + price;
+        priceMessage = priceMessage + "\nThank you";
         displayMessage(priceMessage);
+    }
+
+    /**
+     * Calculate the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
@@ -40,36 +56,38 @@ public class MainActivity<displayPrice> extends AppCompatActivity {
 
 
     /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-    /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
         quantity = 1 + quantity;
         display(quantity);
-        //displayPrice(quantity * 5);
     }
+
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
-        //displayPrice(quantity * 5);
+        int someQuantity = 100;
+        displayQuantity(someQuantity);
+    }
+
+    private void displayQuantity(int numero) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + numero);
+    }
+
+
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);  // cos parameter is integer - this line or ->
+        // -> this - quantityTextView.setText(String.valueOf(number));
+
     }
 
 
