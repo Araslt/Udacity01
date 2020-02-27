4defa19aed198e9638797b10196ab1c542923374
ArasLT <buk.sveikas@gmail.com>
2020-02-27 19:27:45 +0200

    final Just Java without styles and strings

--- JustJava/app/src/main/java/lt/mosaica/justjava/MainActivity.java
+++ JustJava/app/src/main/java/lt/mosaica/justjava/MainActivity.java
@@ -2,9 +2,16 @@ package lt.mosaica.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -24,11 +31,42 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
 
-/*        String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);*/
+        //Find the user's name
+        EditText nameEditText = (EditText) findViewById(R.id.name_field);
+        String name = nameEditText.getText().toString();
+        Log.v("MainActivity", "Name: ");
+
+        //Figure out if the user wants whipped cream toppings
+        CheckBox whippedCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCheckBox.isChecked();
+        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
+
+        //Figure out if the user wants chocolate toppings
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        Log.v("MainActivity", "Has Chocolate: " + hasChocolate);
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = (createOrderSummary(name, price, hasWhippedCream, hasChocolate));
+        //displayMessage(createOrderSummary(name, price, hasWhippedCream, hasChocolate));
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        Log.v("MainActivity", "The price is: " + price);
+
+/*        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:47.6,-122.3?z=11"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }*/
+
     }
 
     /**
@@ -42,11 +80,26 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Calculate the price of the order.
-     *
+     * @param addWhippedCream is wheter or not the user wants whipped cream topping
+     * @param addChocolate is whether or not the user wants chocolate topping
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        //Price of 1 cup of coffee
+        int basePrice = 5;
+
+        //add $1 if user wants whipped cream
+        if (addWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+
+        //add $2 if user wants chocolate
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+
+        //calculate the total order price multiply by quantity
+        return quantity * basePrice;
 
     }
 
@@ -54,10 +107,18 @@ public class MainActivity extends AppCompatActivity {
      * Create summary of the order.
      *
      * @param price of the order
+     * @param name of the customer
+     * @param addWhippedCream is wheter or not the user wants whipped cream topping
+     * @param addChocolate is whether or not the user wants chocolate topping
      * @return total price
+     *
+     * metodas kuris keliauja i/is submitOrder() is kur ir pasiims visus cia isvardintus parametrus
      */
-    private String createOrderSummary(int price) {
-        String priceMessage = "Name: Captain Kunal";
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nadd whipped cream? " + addWhippedCream;
+        priceMessage += "\nadd chocolate? " + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal $" + price;
         priceMessage += "\nThank you";
@@ -68,33 +129,37 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 10) {
+            Toast.makeText(this, "You cannot have more than 10 cups", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = 1 + quantity;
         display(quantity);
+        Log.v("MainActivity", "added one more cup: " + quantity);
     }
 
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast toast = Toast.makeText(this, "You cannot have less than 1 cups", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+            return;
+        }
         quantity = quantity - 1;
-        int someQuantity = 100;
-        displayQuantity(someQuantity);
+        display(quantity);
+        Log.v("MainActivity", "minus one cup coffee" + quantity);
     }
-
-    private void displayQuantity(int numero) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + numero);
-    }
-
-
-
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);  // cos parameter is integer - this line or ->
+        quantityTextView.setText("" + number);
+        // cos parameter is integer - this line or ->
         // -> this - quantityTextView.setText(String.valueOf(number));
 
     }
