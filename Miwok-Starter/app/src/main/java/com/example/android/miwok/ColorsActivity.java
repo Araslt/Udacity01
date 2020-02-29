0f7c50df554d6803d200d2fb2c98504bfd7d3505
ArasLT <buk.sveikas@gmail.com>
2020-02-29 20:11:57 +0200

    miwok.app su onClick is .xml ir intent MainActivity

---
+++ Miwok-Starter/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.miwok;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.miwok.R;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+    }
+}
---
+++ Miwok-Starter/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+/*        NumberClickListeners clickListener = new NumberClickListeners();
+        //find the view that shows the number category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        //set  a clicklistener on that view
+        numbers.setOnClickListener(clickListener);*/
+
+    }
+
+    public void openNumbersList(View view) {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+}
