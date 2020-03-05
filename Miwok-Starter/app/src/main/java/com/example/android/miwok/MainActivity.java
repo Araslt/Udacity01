67ce0fdd7924a6e0dc1d107f9d1a3825583fd390
ArasLT <buk.sveikas@gmail.com>
2020-03-05 19:50:10 +0200

    miwok. added string and onClickListener

--- Miwok-Starter/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok-Starter/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -16,12 +16,62 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        NumberClickListeners clickListener = new NumberClickListeners();
-        //find the view that shows the number category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        //set  a clicklistener on that view
-        numbers.setOnClickListener(clickListener);
+        // Find the view that shows the number category
+        TextView numbers = findViewById(R.id.numbers);
 
+        // Set  a clicklistener on that view
+        numbers.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //create a new intent to open a {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Find the view that shows the family category
+        TextView family = findViewById(R.id.family);
+
+        // Set  a clicklistener on that view
+        family.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //create a new intent to open a {@link FamilyActivity}
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        // Find the view that shows the family category
+        TextView phrases = findViewById(R.id.phrases);
+
+        // Set  a clicklistener on that view
+        phrases.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //create a new intent to open a {@link PhrasesActivity}
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+        // Find the view that shows the Colors category
+        TextView colors = findViewById(R.id.colors);
+
+        // Set  a clicklistener on that view
+        colors.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //create a new intent to open a {@link ColorsActivity}
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
     }
 
 }
+
--- Miwok-Starter/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok-Starter/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -9,6 +11,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = String.valueOf(2^2);
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumbersActivity", "word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "word at index 0: " + words[1]);
     }
 
 }
